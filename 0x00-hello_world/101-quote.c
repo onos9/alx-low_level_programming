b4c09caeb6eb4b31dec206bc898f2875530d8eb5
Onojeta Brown <onosbrown.saved@gmail.com>
2023-02-19 07:46:42 +0000

    Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,10 @@
  */
 int main(void)
 {
-       	const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	const int len = strlen(msg);
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int len = strlen(msg);
+
 	write(2, msg, len);
+
 	return (1);
 }
